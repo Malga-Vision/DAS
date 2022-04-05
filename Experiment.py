00d836d0d5e76c3a3fff1edbdd3047dcdaf89893
francescomontagna <francesco.montagna997@gmail.com>
2022-04-05 14:54:52 +0200

    Cleaned up experiments setting

--- Experiment.py
+++ Experiment.py
@@ -22,7 +22,7 @@ class Experiment(metaclass=abc.ABCMeta):
         raise NotImplementedError
 
     @abc.abstractmethod
-    def config_logs(self):
+    def config_logs(self, run_logs):
         """Logs of a tested configuration"""
         raise NotImplementedError
 
@@ -67,7 +67,7 @@ class CAMExperiment(Experiment):
         
 
     def run_config(self, params, N, eta_G, eta_H):
-        d = params[0] # Fix
+        d = params['d']
         if self.s0 == 'd':
             s0 = d
         elif self.s0 == '4d':
@@ -86,24 +86,25 @@ class CAMExperiment(Experiment):
         self.config_logs(run_logs)
 
     
-    def run_experiment(N, eta_G, eta_H):
+    def run_experiment(self, N, eta_G, eta_H):
         start = time.time()
         run = 0
         tot_runs = len(self.d_values)
-        for d in self.d_values:
+        for d in list(ParameterGrid({'d': self.d_values})):
             self.run_config(d, N, eta_G, eta_H)
-            self..save_logs()
+            self.save_logs()
 
             run += 1
             print(f"Run {run}/{tot_runs} ------ {round(time.time() - start, 2)}s ------")
 
 
 class SteinFastExperiment(Experiment):
-    def __init__(self, d_values, num_tests, s0, thresholds, output_file):
+    def __init__(self, d_values, num_tests, s0, thresholds, pruning, output_file):
         self.d_values = d_values
-        self.num_tests = num_test
+        self.num_tests = num_tests
         self.s0 = s0
         self.thresholds = thresholds
+        self.pruning = pruning
 
         self.logs = []
         self.columns = [
@@ -111,76 +112,87 @@ class SteinFastExperiment(Experiment):
         ]
         self.output_file = output_file
 
-    def run_config(self, d, N, eta_G, eta_H):
-        pass
-
-
-
-    def run(self, d, s0, N, eta_G, eta_H, cam_cutoff=None, threshold=None):
-        run_logs = []
-        for k in range(self.num_tests):        
-            X, adj = generate(d, s0, N, GP=True)
-
-            A_SCORE, top_order_SCORE, SCORE_time, tot_time =  SCORE(X, eta_G, eta_H, cam_cutoff, pruning=self.pruning, threshold = threshold)
-
-            fn, fp, rev = edge_errors(A_SCORE, adj)
-            SHD = sum((fn, fp, rev))
-            SID = int(cdt.metrics.SID(target=adj, pred=A_SCORE))
-            top_order_errors = num_errors(top_order_SCORE, adj)
-
-            run_logs.append([d, s0, N, threshold, fn, fp, rev, SHD, SID, top_order_errors, SCORE_time, tot_time])
-
-            mean_logs = np.mean(run_logs, axis=0)
-            std_logs = np.std(run_logs, axis=0)
-            logs = []
-            for i in range(len(self.columns)):
-                m = mean_logs[i]
-                s = std_logs[i]
-                if self.columns[i] in ["V", "E", "N"]:
-                    logs.append(f"{int(m)}")
-                elif self.columns[i] == "threshold":
-                    logs.append(f"{round(m, 2)}")
-                else:
-                    logs.append(f"{round(m, 2)} +- {round(s, 2)}")
-        
+    def config_logs(self, run_logs):
+        mean_logs = np.mean(run_logs, axis=0)
+        std_logs = np.std(run_logs, axis=0)
+        logs = []
+        for i in range(len(self.columns)):
+            m = mean_logs[i]
+            s = std_logs[i]
+            if self.columns[i] in ["V", "E", "N"]:
+                logs.append(f"{int(m)}")
+            elif self.columns[i] == 'threshold':
+                logs.append(round(m, 2))
+            else:
+                logs.append(f"{round(m, 2)} +- {round(s, 2)}")
         self.logs.append(logs)
 
 
-def stein_fast_exp(num_tests, N, eta_G, eta_H):
-    # Hyperparameters
-    thresholds = [0.05, 0.1, 0.15, 0.2, 0.25]
-    d_values= [10, 20, 50, 100]
-    param_grid = {'d': d_values, 'threshold': thresholds}
-    params = list(ParameterGrid(param_grid))
+    def run_config(self, params, N, eta_G, eta_H):
+        d = params['d']
+        threshold = params['threshold']
+        if self.s0 == 'd':
+            s0 = d
+        elif self.s0 == '4d':
+            s0 = 4*d
+        else:
+            raise ValueError("Forbidden s0 value")
 
-    run = 0
-    tot_runs = len(params)*2
-    start = time.time()
+        run_logs = []
+        for k in range(self.num_tests):
+            X, adj = generate(d, s0, N, GP=True)
 
-    for pruning in ["Fast"]:
-        experiments = Experiment(num_tests, pruning)
-        for args in params:
-            d = args['d']
-            s0 = d # TODO Fix! Test con s0 fixed at 10, and s0 at 4*d
-            threshold = args['threshold']
-            experiments.run(d, s0, N, eta_G, eta_H, threshold=threshold)
-            experiments.save_logs()
+            A_SCORE, top_order_SCORE, SCORE_time, tot_time =  SCORE(X, eta_G, eta_H, pruning=self.pruning, threshold=threshold)
+            fn, fp, rev, SHD, SID, top_order_errors = self.metrics(A_SCORE, adj, top_order_SCORE)
+            run_logs.append([d, s0, N, threshold, fn, fp, rev, SHD, SID, top_order_errors, SCORE_time, tot_time])
+
+        self.config_logs(run_logs)
+
+    def run_experiment(self, N, eta_G, eta_H):
+        start = time.time()
+        param_grid = list(ParameterGrid({'d': self.d_values, 'threshold': self.thresholds}))
+        run = 0
+        tot_runs = len(param_grid)
+        for params in param_grid:
+            self.run_config(params, N, eta_G, eta_H)
+            self.save_logs()
 
             run += 1
             print(f"Run {run}/{tot_runs} ------ {round(time.time() - start, 2)}s ------")
 
 
-def cam_exp(num_tests, N, eta_G, eta_H, cam_cutoff):
-    d_values = [10, 20, 50]
+if __name__ == "__main__":
+    """
+    Run both s0=d and s0=4*d experiments. Logs in different files
+    """
+    ########## COMMON PARAMETERS ##########
+    # Regression
+    N = 1000
+    eta_G = 0.001
+    eta_H = 0.001
 
-    run = 0
-    tot_runs = len(d_values)
-    start = time.time()
-    experiments = Experiment(num_tests, "CAM")
-    for d in d_values:
-        s0 = d
-        experiments.run(d, s0, N, eta_G, eta_H, cam_cutoff, 0)
-        experiments.save_logs()
+    # Iterations for average and standard devation
+    num_tests = 10
 
-        run += 1
-        print(f"Run {run}/{tot_runs} ------ {round(time.time() - start, 2)}s ------")
+    # Pruning algorithm: ["Fast", "Stein", "CAM"]
+    pruning = "Fast"
+
+    if pruning == "Fast" or pruning == "Stein":
+        d_values = [50, 100, 200]
+        thresholds = [0.05, 0.1, 0.15, 0.2, 0.25]
+        for s0 in ['4d']:
+            output_file = f"logs_{pruning.lower()}_{s0}_er.csv"
+            experiment = SteinFastExperiment(d_values, num_tests, s0, thresholds, pruning, output_file)
+            experiment.run_experiment(N, eta_G, eta_H)
+
+    elif pruning == "CAM":
+        d_values = [10, 20, 50]
+        cam_cutoff=0.001
+
+        for s0 in ['4d']:
+            output_file = f"logs_cam_{s0}_er.csv"
+            experiment = CAMExperiment(d_values, num_tests, s0, cam_cutoff, output_file)
+            experiment.run_experiment(N, eta_G, eta_H)
+
+    else:
+        raise ValueError("Unknown pruning method")
--- main.py
+++
@@ -1,7 +0,0 @@
-from args import get_args
-
-def main():
-    args = get_args()
-
-if __name__ == "__main__":
-    main()
