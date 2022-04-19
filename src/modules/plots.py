221680b9f4e9d63d566518e34b0af7cf459e9d6e
francescomontagna <francesco.montagna997@gmail.com>
2022-04-19 12:34:17 +0200

    mean-median plot, time-execution plot

---
+++ src/modules/plots.py
@@ -0,0 +1,70 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+def execution_time(input_path, output_path):
+  sns.set_theme(style="ticks")
+  data = pd.read_csv(input_path)
+  data['time'] = data['time'].apply(lambda x: float(x.split(' ')[0]))
+
+  f, (ax_top, ax_bottom) = plt.subplots(ncols=1, nrows=2, sharex=True, gridspec_kw={'hspace':0.2})
+  sns.barplot(x="V", y="time", hue="pruning", data=data, ax=ax_top)
+  sns.barplot(x="V", y="time", hue="pruning", data=data, ax=ax_bottom)
+
+  # y-axis
+  top_min, top_max = (500, 5500)
+  sns.despine(ax=ax_bottom)
+  sns.despine(ax=ax_top, bottom=True)
+  ax_bottom.set_ylabel("Time[s]")
+  ax_top.set_ylabel("")
+  ax_top.set_ylim(top_min, top_max)   
+  ax_bottom.set_ylim(0, 300)
+  ax_top.set_yticks(np.arange(top_min, top_max, 1000))
+
+  # x-axis
+  ax_top.get_xaxis().set_visible(False) # Remove x-axis from top graph
+  ax_bottom.set_xlabel("Nodes")
+
+  ax_bottom.get_legend().remove()
+  ax_top.get_legend().remove()
+  # then create a new legend and put it to the side of the figure (also requires trial and error)
+  ax_top.legend(loc=(1.025, 0.5), title="Pruning algorithm")
+
+  ax = ax_top
+  d = .015  # how big to make the diagonal lines in axes coordinates
+  kwargs = dict(transform=ax.transAxes, color='k', clip_on=False)
+  ax.plot((-d, +d), (-d, +d), **kwargs)        # top-left diagonal
+  ax2 = ax_bottom
+  kwargs.update(transform=ax2.transAxes)  # switch to the bottom axes
+  ax2.plot((-d, +d), (1 - d, 1 + d), **kwargs)  # bottom-left diagonal
+
+  plt.savefig(output_path, bbox_inches='tight')
+
+
+def mean_vs_median(input_path, output_paths):
+  """
+  Compare SHD and SID score between mean and median. Comparison across [0.05, 0.1, 0.15, 0.2] threshold values
+  """
+  sns.set_theme(style="ticks")
+  data = pd.read_csv(input_path)
+  data['SHD'] = data['SHD'].apply(lambda x: float(x.split(' ')[0]))
+  data['SID'] = data['SID'].apply(lambda x: float(x.split(' ')[0]))
+
+  g = sns.catplot(data=data, x='V', y='SHD', hue='Statistics', col='Threshold', kind='bar')
+  g.set(ylim=(0, 300))
+
+  h = sns.catplot(data=data, x='V', y='SID', hue='Statistics', col='Threshold', kind='bar')
+
+  def save_figure(sns_plot, path):
+    fig = sns_plot.get_figure()
+    fig.save_fig(path)
+
+  save_figure(g, output_paths[0])
+  save_figure(h, output_paths[1])
+
+
+def algorithm_comparison(input_path, output_path):
+  """
+  """
+  pass
---
+++ src/visualization.py
@@ -0,0 +1,6 @@
+from modules.plots import *
+
+input_path = "../plot_data/execution_times.csv"
+output_path = "../plots/exe_time.png"
+
+execution_time(input_path, output_path)
